ea52cf614100b95ba8ba7853ea10a09e932ee477
HelixInteractive <161367758+HelixInteractive@users.noreply.github.com>
2024-02-29 20:40:40 +0000

    Add code to FirstProgram.cpp

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -1,1 +1,15 @@
 //This is our first program
+
+int main() {
+  int mainIn = 0;
+  
+  cout << 'Enter an integer > ';
+  cin >> mainIn;
+  cout << mainIn;
+  if(mainIn < 0){
+    cout << 'Your number was negative';
+  }else{
+    cout << 'Your number was positive';
+  }
+    
+}
